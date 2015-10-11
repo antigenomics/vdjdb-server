19c76b4b9296fe08c36743dd1d1abd230baa1720
bvdmitri <bvdmitri@gmail.com>
2015-10-11 17:19:21 +0300

    search page update

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -8,26 +8,43 @@
         PATTERN: 'pattern'
     });
 
-    app.factory('token', ['$hhtp', '$log', function($hhtp, $log) {
+    app.directive('onLastRepeat', function() {
+        return function(scope, element, attrs) {
+            if (scope.$last) setTimeout(function(){
+                scope.$emit('onRepeatLast', element, attrs);
+            }, 1);
+        };
+    });
+
+    app.factory('token', ['$http', '$log', function($hhtp, $log) {
 
     }]);
 
-    app.factory('filters', ['$http', '$log', function($http, $log) {
+    app.factory('searching', ['$http', '$log', function($http, $log) {
         var filters = [];
         var availableFields = [];
+        var results = [];
+        var noResults = false;
 
         $http.get('/api/getAvailableFields')
             .success(function(data) {
-                angular.copy(data, availableFields);
+                angular.forEach(data, function(field) {
+                    availableFields.push({
+                        name: field[1],
+                        db_name: field[0]
+                    })
+                })
         });
 
         function search() {
+            noResults = false;
             $http.post('/api/search', {
                 filters: filters,
                 token: '78879f71-48c5-4fc5-9e12-3015b7b50dcf'
             })
-                .success(function(response) {
-                    $log.info(response);
+                .success(function(data) {
+                    if (data.length == 0) noResults = true;
+                    angular.copy(data, results);
                 })
                 .error(function(error) {
                     $log.error(error);
@@ -44,19 +61,36 @@
             })
         }
 
+
+        // ---------------------------------------------- //
+        // Getters
+        // ---------------------------------------------- //
+
         function getFitlers() {
             return filters;
+        }
+
+        function getResults() {
+            return results;
         }
 
         function getAvailableFields() {
             return availableFields;
         }
 
+        function isNoResults() {
+            return noResults;
+        }
+
+        // ---------------------------------------------- //
+
         return {
             addFilter: addFilter,
+            search: search,
             getFilters: getFitlers,
             getAvailableFields: getAvailableFields,
-            search: search
+            getResults: getResults,
+            isNoResults: isNoResults
         }
     }]);
 
@@ -64,11 +98,14 @@
     app.directive('search', function() {
         return {
             restrict: 'E',
-            controller: ['$scope', 'filters', function($scope, filters) {
-                $scope.filters = filters.getFilters();
-                $scope.availableFields = filters.getAvailableFields();
-                $scope.addFilter = filters.addFilter;
-                $scope.search = filters.search;
+            controller: ['$scope', 'searching', function($scope, searching) {
+                $scope.filters = searching.getFilters();
+                $scope.results = searching.getResults();
+
+                $scope.availableFields = searching.getAvailableFields();
+                $scope.addFilter = searching.addFilter;
+                $scope.search = searching.search;
+                $scope.isNoResults = searching.isNoResults;
 
                 $scope.logFilters = function() {
                     console.log($scope.filters);
@@ -90,3 +127,25 @@
     })
 
 })();
+
+function testWatchers() {
+    "use strict";
+    var root = $(document.getElementsByTagName('body'));
+    var watchers = [];
+
+    var f = function (element) {
+        if (element.data().hasOwnProperty('$scope')) {
+            angular.forEach(element.data().$scope.$$watchers, function (watcher) {
+                watchers.push(watcher);
+            });
+        }
+
+        angular.forEach(element.children(), function (childElement) {
+            f($(childElement));
+        });
+    };
+
+    f(root);
+
+    return watchers.length;
+}
