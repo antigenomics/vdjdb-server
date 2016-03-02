b277357216886e2c36557f488dd7aaa57713919a
Bagaev Dmitri <bvdmitri@gmail.com>
2016-03-03 01:21:21 +0300

    some improvments

--- app/server/wrappers/IntersectResult.java
+++ app/server/wrappers/IntersectResult.java
@@ -23,13 +23,48 @@ public class IntersectResult {
         }
     }
 
-    public String cdr3aa;
+    public class CDR3Wrapper {
+        public String cdr3aa;
+        public String cdr3nt;
+        public int vend;
+        public int jstart;
+        public int dstart;
+        public int dend;
+
+        public CDR3Wrapper(Clonotype clonotype) {
+            cdr3aa = clonotype.getCdr3aa();
+            cdr3nt = clonotype.getCdr3nt();
+            vend = clonotype.getVEnd();
+            jstart = clonotype.getJStart();
+            dstart = clonotype.getDStart();
+            dend = clonotype.getDEnd();
+        }
+    }
+
+    public class ClonotypeWrapper {
+        public CDR3Wrapper cdr;
+        public String v;
+        public String j;
+        public Long count;
+        public Double freq;
+
+        public ClonotypeWrapper(Clonotype clonotype) {
+            cdr = new CDR3Wrapper(clonotype);
+            v = clonotype.getV();
+            j = clonotype.getJ();
+            count = clonotype.getCount();
+            freq = clonotype.getFreq();
+        }
+
+    }
+
+    public ClonotypeWrapper clonotype;
     public List<AlignmentHelperResult> alignmentHelperList = new ArrayList<>();
 
     public IntersectResult(Clonotype clonotype, List<ClonotypeSearchResult> clonotypeSearchResults) {
-        cdr3aa = clonotype.getCdr3aa();
+        this.clonotype = new ClonotypeWrapper(clonotype);
         for (ClonotypeSearchResult clonotypeSearchResult : clonotypeSearchResults) {
-            alignmentHelperList.add(new AlignmentHelperResult(clonotypeSearchResult));
+            this.alignmentHelperList.add(new AlignmentHelperResult(clonotypeSearchResult));
         }
     }
 }
--- public/javascripts/annotations/annotations.js
+++ public/javascripts/annotations/annotations.js
@@ -106,60 +106,175 @@
 
 
 function intersectResultsTable(data) {
-    var results = [];
+    var clonotypes = [];
     angular.forEach(data, function (result) {
-        results.push([result.cdr3aa, result]);
+        //TODO code must be simple, maybe push clonotype itself
+        var cdr = cdr3Transform(result.clonotype.cdr);
+        clonotypes.push({
+            freq: result.clonotype.freq,
+            count: result.clonotype.count,
+            cdr3aa: cdr.cdr3aa,
+            cdr3nt: cdr.cdr3nt,
+            v: result.clonotype.v,
+            j: result.clonotype.j,
+            helpers: result.alignmentHelperList
+        });
     });
 
-    var d3Place = d3.select("#intersect_report_table");
-        d3Place.html("");
-    var table = d3Place.append("table")
-        .attr("id", "intersect_table")
-        .attr("class", "table table-hover compact");
-    var thead = table.append("thead").append("tr");
-    var columns = [{
-        title: 'cdr3aa'
-    }];
+    var columns = [
+        {
+            className:      'details-control',
+            orderable:      false,
+            data:           null,
+            defaultContent: '',
+            width: '5%'
+        },
+        { data: 'freq', width: '5%' },
+        { data: 'count', width: '5%' },
+        { data: 'cdr3aa' },
+        { data: 'v' },
+        { data: 'j' },
+        { data: 'cdr3nt'}
+    ];
 
-    thead.append("th").html("CDR3aa");
-
-    var dataTable = $('#intersect_table').dataTable({
-        data: results,
+    var dataTable = $('#intersect_report_table').DataTable({
+        data: clonotypes,
         columns: columns,
-        dom: '<"pull-left"l><"clear">Trtd<"pull-left"i>p',
-        responsive: true,
         order: [
-            [0, "desc"]
-        ],
-        iDisplayLength: 50,
-        scrollY: "350px",
-        "fnRowCallback": function(nRow, aData) {
-            $(nRow).click(function() {
-                intersectResultReport(aData[1].alignmentHelperList)
-            });
-        }
+            [1, 'desc']
+        ]
     });
+
+    function format(d) {
+        var helpers = d.helpers;
+        var addInfo = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px; background-color: #DCDCDC">';
+        addInfo += '<tr><td>Matches: </td></tr>';
+        angular.forEach(helpers, function(helper) {
+            addInfo += '<tr>' +
+                    '<td class="alignment_block">' +
+                        '<p class="alignment_text">' +
+                            helper.alignmentHelper.seq1String +
+                        '</p>' +
+                        '<p class="alignment_text">' +
+                            helper.alignmentHelper.markup +
+                        '</p>' +
+                        '<p class="alignment_text">' +
+                            helper.alignmentHelper.seq2String +
+                        '</p>' +
+                        '<hr>' +
+                    '</td>' +
+                '</tr>'
+        });
+        addInfo += '</table>';
+        return addInfo;
+    }
+
+    $('#intersect_report_table tbody').on('click', 'td.details-control', function () {
+        var tr = $(this).closest('tr');
+        var row = dataTable.row( tr );
+
+        if ( row.child.isShown() ) {
+            // This row is already open - close it
+            row.child.hide();
+            tr.removeClass('shown');
+        }
+        else {
+            // Open this row
+            console.log(row.data());
+            row.child( format(row.data()) ).show();
+            tr.addClass('shown');
+        }
+    } );
 }
 
-function intersectResultReport(alignmentHelperList) {
-    console.log(alignmentHelperList);
-    var place = d3.select("#intersect_report");
-        place.html("");
-    angular.forEach(alignmentHelperList, function(helper) {
-        var helperPlace = place.append("div").attr("class", "alignment_block");
-        var table = helperPlace.append("table").attr("class", "table");
-        var thead = table.append("thead").append("tr");
-        var tbody = table.append("tbody").append("tr");
-        angular.forEach(helper.row.entries, function(entry) {
-            thead.append("th").text(entry.column.name);
-            tbody.append("td").text(entry.value);
-        });
+function createSubstring(cdr, start, end, color) {
+    "use strict";
+    return {
+        start: start,
+        end: end,
+        color: color,
+        substring: cdr.substring(start, end + 1)
+    };
+}
 
-        helperPlace.append("p").attr("class", "alignment_text").text(helper.alignmentHelper.seq1String);
-        helperPlace.append("p").attr("class", "alignment_text").text(helper.alignmentHelper.markup);
-        helperPlace.append("p").attr("class", "alignment_text").text(helper.alignmentHelper.seq2String);
-        helperPlace.append("hr");
-    });
+function cdr3Transform(cdr) {
+    "use strict";
+    var cdr3aa = cdr.cdr3aa,
+        cdr3nt = cdr.cdr3nt,
+        vend_nt = cdr.vend,
+        dstart_nt = (cdr.dstart < 0) ? vend_nt + 1 : cdr.dstart,
+        dend_nt = (cdr.dend < 0) ? vend_nt : cdr.dend,
+        jstart_nt = (cdr.jstart < 0) ? 10000 : cdr.jstart,
+        vend_aa = Math.floor(cdr.vend / 3),
+        dstart_aa = (Math.floor(cdr.dstart / 3) < 0) ? vend_aa + 1 : Math.floor(cdr.dstart / 3),
+        dend_aa = (Math.floor(cdr.dend / 3) < 0) ? vend_aa : Math.floor(cdr.dend / 3),
+        jstart_aa = (Math.floor(cdr.jstart / 3) < 0) ? 10000 : Math.floor(cdr.jstart / 3);
+
+    var cdr3nt_arr = [],
+        cdr3aa_arr = [];
+
+    while (vend_nt >= jstart_nt) jstart_nt++;
+    while (vend_aa >= jstart_aa) jstart_aa++;
+    while (dstart_nt <= vend_nt) dstart_nt++;
+    while (dstart_aa <= vend_aa) dstart_aa++;
+    while (dend_nt >= jstart_nt) dend_nt--;
+    while (dend_aa >= jstart_aa) dend_aa--;
+
+    if (vend_nt >= 0) {
+        cdr3nt_arr.push(createSubstring(cdr3nt, 0, vend_nt, "#4daf4a"));
+    }
+
+    if (vend_aa >= 0) {
+        cdr3aa_arr.push(createSubstring(cdr3aa, 0, vend_aa, "#4daf4a"));
+    }
+
+    if (dstart_nt - vend_nt > 1) {
+        cdr3nt_arr.push(createSubstring(cdr3nt, vend_nt + 1, dstart_nt - 1, "black"));
+    }
+
+    if (dstart_aa - vend_aa > 1) {
+        cdr3aa_arr.push(createSubstring(cdr3aa, vend_aa + 1, dstart_aa - 1, "black"));
+    }
+
+    if (dstart_nt > 0 && dend_nt > 0 && dend_nt >= dstart_nt) {
+        cdr3nt_arr.push(createSubstring(cdr3nt, dstart_nt, dend_nt, "#ec7014"));
+    }
+
+    if (dstart_aa > 0 && dend_aa > 0 && dend_aa >= dstart_aa) {
+        cdr3aa_arr.push(createSubstring(cdr3aa, dstart_aa, dend_aa, "#ec7014"));
+    }
+
+    if (jstart_nt - dend_nt > 1) {
+        cdr3nt_arr.push(createSubstring(cdr3nt, dend_nt + 1, jstart_nt - 1, "black"));
+    }
+
+    if (jstart_aa - dend_aa > 1) {
+        cdr3aa_arr.push(createSubstring(cdr3aa, dend_aa + 1, jstart_aa - 1, "black"));
+    }
+
+    if (jstart_nt > 0) {
+        cdr3nt_arr.push(createSubstring(cdr3nt, jstart_nt, cdr3nt.length, "#377eb8"));
+    }
+
+    if (jstart_aa > 0) {
+        cdr3aa_arr.push(createSubstring(cdr3aa, jstart_aa, cdr3aa.length, "#377eb8"));
+    }
+
+    var cdr3nt_result = "", element, i = 0;
+    for (i = 0; i < cdr3nt_arr.length; i++) {
+        element = cdr3nt_arr[i];
+        cdr3nt_result += '<text style="color: ' + element.color + '">' + element.substring + '</text>';
+    }
+    var cdr3aa_result = "";
+    for (i = 0; i < cdr3aa_arr.length; i++) {
+        element = cdr3aa_arr[i];
+        cdr3aa_result += '<text style="color: ' + element.color + '">' + element.substring + '</text>';
+    }
+
+    cdr.cdr3aa = cdr3aa_result;
+    cdr.cdr3nt = cdr3nt_result;
+
+    return cdr;
 }
 
 function testWatchers() {
